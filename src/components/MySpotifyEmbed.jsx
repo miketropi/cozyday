461209f990551af9ec31e78ed2ce17866ae35519
Mike <mike.beplus@gmail.com>
2025-05-10 11:20:35 +0700

    Update

--- src/components/MySpotifyEmbed.jsx
+++ src/components/MySpotifyEmbed.jsx
@@ -4,16 +4,32 @@ export default function MySpotifyEmbed({ uri }) {
 
   useEffect(() => {
     const script = document.createElement('script')
-    script.src = 'https://sdk.scdn.co/spotify-player.js'
+    script.src = 'https://open.spotify.com/embed/iframe-api/v1'
     script.async = true
     document.body.appendChild(script)
 
+    window.onSpotifyIframeApiReady = (IFrameAPI) => {
+      const element = document.getElementById('spotify-player');
+      const options = {
+        uri: uri,
+        width: '100%',
+        height: 80,
+        autoplay: true
+      };
+      
+      const callback = (EmbedController) => {
+   
+      };
+      
+      IFrameAPI.createController(element, options, callback);
+    };
+    
     return () => {
       document.body.removeChild(script)
     }
   }, [])
 
   return (
-    <iframe style={{ borderRadius: '12px' }} src={uri} width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+    <iframe id="spotify-player" style={{ borderRadius: '12px' }} src={uri} width="100%" height="80" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
   )
 }
